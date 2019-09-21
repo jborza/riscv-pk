406aed18c2a3d6bb924ebd1f749c8e1863df29ea
jborza <jborza@gmail.com>
2019-09-21 14:58:23 +0200

    more portable build script

--- build/build.sh
+++
@@ -1,4 +0,0 @@
-make -j 4 CFLAGS='-g -O0' bbl
-cp bbl bbl32.elf
-/opt/riscv32i/bin/riscv32-unknown-elf-objcopy -O binary bbl32.elf bbl32.bin
-/opt/riscv32i/bin/riscv32-unknown-elf-objdump -dS bbl32.elf > bbl32.s
---
+++ configure-debug.sh
@@ -0,0 +1,3 @@
+../configure --prefix=$RISCV --with-arch=rv32i --host=riscv32-unknown-elf \
+--disable-vm --enable-print-device-tree --enable-boot-machine --enable-logo \
+--enable-debug-information
